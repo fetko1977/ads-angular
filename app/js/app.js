e318a4a13b681d77f469f059cae208664d58868b
fetko1977 <feddfriend77@gmail.com>
2014-12-28 20:06:46 +0200

    Added routing for register.html and for login.html.

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,14 @@
-
+var adsApp = angular.module('adsApp', ['ngRoute'])
+.config(function($routeProvider){
+        $routeProvider.when('/login',
+            {
+                templateUrl:'templates/login.html'
+            }
+        )
+            .when('/register',
+            {
+                templateUrl:'templates/register.html'
+            }
+        )
+    });
 
---
+++ app/js/controllers/mainController.js
@@ -0,0 +1,3 @@
+adsApp.controller('MainController', function($scope){
+    // TODO ....
+});
