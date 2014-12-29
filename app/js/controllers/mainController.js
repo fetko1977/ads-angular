41e6269bd8fc96585ab5c2c51df255c35582fd70
fetko1977 <feddfriend77@gmail.com>
2014-12-29 18:07:47 +0200

    Added categories and towns.

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,5 +1,11 @@
-adsApp.controller('MainController', function($scope, adsMainData){
-    adsMainData.getAllAds(function(resp){
+adsApp.controller('MainController', function($scope, MainData){
+    MainData.getAllAds(function(resp){
         $scope.data = resp;
+    });
+    MainData.getAdsCategories(function(resp){
+        $scope.categories = resp;
+    });
+    MainData.getAdsTowns(function(resp){
+        $scope.towns = resp;
     });
 });
--- app/js/services/adsData.js
+++ app/js/services/adsData.js
@@ -1,7 +1,25 @@
-adsApp.factory('adsMainData', function($http, $log){
+adsApp.factory('MainData', function($http, $log){
     return {
         getAllAds : function(success){
-            $http.get('http://softuni-ads.azurewebsites.net/api/ads?pagesize=10&startpage=1')
+            $http({method:'GET', url:'http://softuni-ads.azurewebsites.net/api/ads?pagesize=10&startpage=1'})
+                .success(function(data, status, headers, config) {
+                    success(data);
+                }).
+                error(function(data, status, headers, config) {
+                    $log.warn(data);
+                });
+        },
+        getAdsCategories : function(success){
+            $http({method:'GET', url:'http://softuni-ads.azurewebsites.net/api/categories'})
+                .success(function(data, status, headers, config) {
+                    success(data);
+                }).
+                error(function(data, status, headers, config) {
+                    $log.warn(data);
+                });
+        },
+        getAdsTowns : function(success){
+            $http({method:'GET', url:'http://softuni-ads.azurewebsites.net/api/towns'})
                 .success(function(data, status, headers, config) {
                     success(data);
                 }).
